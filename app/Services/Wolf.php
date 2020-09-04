02f378db550c423024134192b2ab1bd9b37fe91d
Mohammad Hassan Abbasi <mh.abbasi2006@gmail.com>
2020-09-04 16:06:19 +0200

    feat: Add Wolf service methods

--- app/Services/Wolf.php
+++ app/Services/Wolf.php
@@ -3,43 +3,129 @@
 
 namespace App\Services;
 use App\Contracts\Packable;
+use App\Wolf as WolfModel;
+use Exception;
 
 
+/**
+ * Class Wolf
+ * Implementing Packable interface
+ * @package App\Services
+ */
 class Wolf implements Packable
 {
 
+    /**
+     * Storing the last used wolf model to reduce number of queries
+     * @var null
+     */
+    private $wolf = null;
+
+    /**
+     * Using WolfPack service
+     * @var null
+     */
+    private $wolfPackHandler = null;
+
+
+    /**
+     * Wolf constructor.
+     * @param WolfPack $wolfPackHandler
+     */
+    public function __construct(WolfPack $wolfPackHandler)
+    {
+        $this->wolfPackHandler = $wolfPackHandler;
+    }
+
+    /**
+     * Store a wolf
+     * @param $payload
+     * @return WolfModel
+     */
     public function store($payload)
     {
-        // TODO: Implement store() method.
+        return $this->wolf = WolfModel::create($payload);
     }
 
-    public function update($packableId, $payload)
+    /**
+     * Update a wolf using payload
+     *
+     * @param $wolfId
+     * @param $payload
+     * @return WolfModel|null
+     */
+    public function update($wolfId, $payload)
     {
-        // TODO: Implement update() method.
+        $wolf = $this->getById($wolfId);
+        $wolf->fill($payload)->save();
+        return $wolf;
     }
 
+    /**
+     * Lit all the wolves stored in database
+     * @return WolfModel[]|[]
+     */
     public function index()
     {
-        // TODO: Implement index() method.
+        return WolfModel::all();
     }
 
-    public function getById($packableId)
+    /**
+     * Get a wolf by Its ID
+     * @param $wolfId
+     * @return WolfModel|null
+     */
+    public function getById($wolfId)
     {
-        // TODO: Implement getById() method.
+        if( $this->wolf !== null && $this->wolf->id == $wolfId ) {
+            return $this->wolf;
+        }
+        return WolfModel::findOrFail($wolfId);
     }
 
-    public function destroy($packableId)
+    /**
+     * Delete a wolf
+     * @param $wolfId
+     * @return bool|null
+     * @throws Exception
+     */
+    public function destroy($wolfId)
     {
-        // TODO: Implement destroy() method.
+        $wolf = $this->getById($wolfId);
+        return $wolf->delete();
     }
 
-    public function addToPack($packableId, $packId)
+    /**
+     * Add a wolf to a pack
+     * @param $wolfId
+     * @param $packId
+     * @return
+     * @throws Exception
+     */
+    public function addToPack($wolfId, $packId)
     {
-        // TODO: Implement addToPack() method.
+        $pack = $this->wolfPackHandler->getById($packId);
+        $wolf = $this->getById($wolfId);
+        if( $pack->wolves->find($wolfId) ) {
+            throw new \Exception( trans("Wolf is already in the pack!"));
+        }
+        return $pack->wolves()->attach($wolf->id);
     }
 
-    public function removeFromPack($packableId, $packId)
+    /**
+     * Remove a wolf from a pock
+     * @param $wolfId
+     * @param $packId
+     * @return
+     * @throws Exception
+     */
+    public function removeFromPack($wolfId, $packId)
     {
-        // TODO: Implement removeFromPack() method.
+        $pack = $this->wolfPackHandler->getById($packId);
+        $wolf = $this->getById($wolfId);
+        if( !$pack->wolves->find($wolfId) ) {
+            throw new \Exception( trans("Wolf was not in the pack!"));
+        }
+        return $pack->wolves()->detach($wolf->id);.
     }
 }
