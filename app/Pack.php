8562cc36c697c7769fee5a5dc0db3674675e0c73
Mohammad Hassan Abbasi <mh.abbasi2006@gmail.com>
2020-09-04 14:28:01 +0200

    Feat: Add Pack and Wolf models

---
+++ app/Pack.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+/**
+ * @OA\Schema(
+ *   schema="Pack",
+ *   type="object",
+ *   required={"name"}
+ * )
+ *
+ */
+class Pack extends Model
+{
+    /**
+     * @OA\Property(property="name", type="string")
+     */
+
+    use SoftDeletes;
+    protected $fillable = ['name'];
+    protected $hidden = ['pivot'];
+
+}
---
+++ app/Wolf.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @OA\Schema(
+ *   schema="Wolf",
+ *   type="object",
+ *   required={"name","gender","birthdate","location"}
+ * )
+ *
+ */
+class Wolf extends Model
+{
+    /**
+     * @OA\Property(property="name", type="string" , example="Rex")
+     * @OA\Property(property="gender", type="string", maxLength=1, example="m")
+     * @OA\Property(property="birthdate", type="string", format="date", example="2020-09-03")
+     * @OA\Property(property="location", type="string", example="51.4483,5.4728")
+     */
+
+    use SoftDeletes;
+
+    protected $fillable = ['name','gender', 'birthdate', 'location'];
+    protected $hidden = ['pivot'];
+
+}
