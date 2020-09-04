02acc57bc561fbd917d4e2fff8d05b81a9cf30ef
Mohammad Hassan Abbasi <mh.abbasi2006@gmail.com>
2020-09-04 17:22:35 +0200

    feat: Add OpenApi annotation to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,23 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="WolfPack Assignment API Documentation",
+     *      description="You can find all the available endpoints over here",
+     *      @OA\Contact(
+     *          email="mh.abbasi2006@gmail.com"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="API Server"
+     * )
+
+     *
+     *
+     */
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
