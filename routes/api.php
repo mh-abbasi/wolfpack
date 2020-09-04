971aa789e15ededb775f05d6b2bd7ccb9cd02cfd
Mohammad Hassan Abbasi <mh.abbasi2006@gmail.com>
2020-09-04 14:32:21 +0200

    Feat: Add routes to api routes file

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'wolves' => 'API\WolfController',
+    'packs' => 'API\PackController',
+]);
+
+Route::prefix('packs')->group(function() {
+    Route::post('/{pack}/addWolf','API\PackController@addWolfToPack');
+    Route::post('/{pack}/removeWolf','API\PackController@removeWolfFromPack');
+});
